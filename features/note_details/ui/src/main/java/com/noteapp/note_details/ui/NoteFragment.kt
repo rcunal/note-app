210964ad07520469f580732051c06e1f393b1916
Rahmi Cemre Ünal <rahmicemreunal@gmail.com>
2023-09-19 14:18:59 +0300

    remove unused line

--- features/note_details/ui/src/main/java/com/noteapp/note_details/ui/NoteFragment.kt
+++ features/note_details/ui/src/main/java/com/noteapp/note_details/ui/NoteFragment.kt
@@ -42,7 +42,6 @@ class NoteFragment : Fragment(R.layout.fragment_note) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        println()
 
         initListeners()
 
